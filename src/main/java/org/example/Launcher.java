e8fd2f3f0c00e7d0d7ad48c617548b1aea50d285
Алексей <aaalexskortoshka@gmail.com>
2026-02-25 00:52:01 +0600

    edit CSS style

--- src/main/java/org/example/Launcher.java
+++ src/main/java/org/example/Launcher.java
@@ -1,9 +1,5 @@
 package org.example;
 
-/**
- * Точка входа для запуска из IDE.
- * Отдельный класс обходит проверку модульной системы JavaFX при старте.
- */
 public class Launcher {
     public static void main(String[] args) {
         MainApp.main(args);
